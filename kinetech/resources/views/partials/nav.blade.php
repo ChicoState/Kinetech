95f9a2416916c74d6e44b3781c318bd8fcd160cc
JordanLaney <11jlaney11@gmail.com>
2017-09-20 01:25:30 -0700

    v1

--- kinetech/resources/views/partials/nav.blade.php
+++ kinetech/resources/views/partials/nav.blade.php
@@ -5,20 +5,20 @@
 		aria-expanded="false" aria-label="Toggle navigation">
 	<span class="navbar-toggler-icon"></span>
 </button>
-<a class="navbar-brand" href="/">Kinetech</a>
+<a class="text-white navbar-brand" href="/">Kinetech</a>
 <div class="collapse navbar-collapse" id="navbarNav">
 	<ul class="navbar-nav">
 		<li class="nav-item">
-			<a class="nav-link" href="/">Home</a>
+			<a class="text-white link nav-link" href="/">Home</a>
 		</li>
 		<li class="nav-item">
-			<a class="nav-link" href="/about">About</a>
+			<a class="text-white link nav-link" href="/"></a>
 		</li>
 		<li class="nav-item">
-			<a class="nav-link"></a>
+			<a class="text-white link nav-link"></a>
 		</li>
 		<li class="nav-item">
-			<a class="nav-link disabled"></a>
+			<a class="text-white link nav-link disabled"></a>
 		</li>
 	</ul>
 </div>
