f4f99ee6f9d5bc255b374e477b4a8d7d70390565
JordanLaney <11jlaney11@gmail.com>
2017-10-30 00:34:07 -0700

    Email Module, beta

---
+++ kinetech/resources/views/emails/generalcontact.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test Email</title>
+</head>
+<body>
+    <h1>From: {{ $name }} </h1>
+    <h2>Subject: {{ $topic }} </h2>
+    <h3>Content:</h3>
+    <p> {{ $content }} </p>
+</body>
+</html>
