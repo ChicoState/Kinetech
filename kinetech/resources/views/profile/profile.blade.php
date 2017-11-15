5874796806509a943e50098619564a5d3bf53338
Elliott Allmann <elliott.allmann@cox.net>
2017-11-15 12:50:41 -0800

    Use AJAX to trigger user name update.

--- kinetech/resources/views/profile/profile.blade.php
+++ kinetech/resources/views/profile/profile.blade.php
@@ -13,7 +13,7 @@
       </div>
       <div class="row ml-5 mt-2">
           <input class="mb-2" id="ChangeUserInput" style="height:24px; display:none">
-          <button onclick="location.href='/updateProfile';" id="ChangeUserCheck" style="display:none; width:24px; height:24px; color:green">
+          <button onclick="updateInfo()" id="ChangeUserCheck" style="display:none; width:24px; height:24px; color:green">
             <div>&#x2713;</div>
           </button>
           <button id="ChangeUserX" style="display:none; width:24px; height:24px; color:red">
@@ -37,7 +37,24 @@
 </div>
 
 <script>
+    function updateInfo(){
+      var newName = $("#ChangeUserInput").val();
+      var newEmail = $("#ChangeEmailInput").val();
+      $.post('/updateProfile', { "id": {{ $user['id']}},
+                                "username": newName,
+                                "email": newEmail}, 
+                                function(data){
+                                  console.log(data);
+                                }
+      );
+      window.location.reload();
+    }
+
     function changeUser() {
+      $("#ChangeUserInput").toggle();
+      $("#ChangeUserCheck").toggle();
+      $("#ChangeUserX").toggle();
+      /*
         var x = document.getElementById("ChangeUserInput");
         if(x.style.display == "none") {
             x.style.display = "block";
@@ -55,7 +72,7 @@
             z.style.display = "block";
         } else {
             z.style.display = "none";
-        }
+        }*/
     }
     function changeEmail() {
         var x = document.getElementById("ChangeEmailInput");
