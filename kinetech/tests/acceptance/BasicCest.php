03bf36712fc53dbed0142abd652d9ee17f7ff59c
Elliott Allmann <elliott.allmann@cox.net>
2017-11-27 19:12:50 -0800

    Some more basic acceptance testing.

--- kinetech/tests/acceptance/BasicCest.php
+++ kinetech/tests/acceptance/BasicCest.php
@@ -11,6 +11,16 @@ class BasicCest
     {
     }
 
+    private function loginAsTestUser(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->click('LOGIN');
+        $I->wait(1);
+        $I->fillField('email', 'testuser@test.com');
+        $I->fillField('password', 'testpassword');
+        $I->click('Log In');        
+    }
+
     // tests
     public function tryToTest(AcceptanceTester $I)
     {
@@ -23,15 +33,31 @@ class BasicCest
     public function loginTest(AcceptanceTester $I)
     {
         $I->wantTo("Attempt to log in with good data.");
-        $I->amOnPage('/');
-        $I->click('LOGIN');
+        $this->loginAsTestUser($I);
         $I->wait(1);
-        $I->fillField('email', 'testuser@test.com');
-        $I->fillField('password', 'testpassword');
-        $I->click('Log In');
-        $I->wait(2);
         $I->see('CART');
         $I->see('PROFILE');
         $I->see('LOG OUT');
+    }
+
+    public function loadProfileViewTest(AcceptanceTester $I)
+    {
+        $I->wantTo('Log in as a regular user, and then view my profile');
+        $this->loginAsTestUser($I);
+        $I->wait(1);
+        $I->click('PROFILE');
+        $I->wait(1);
+        $I->see('Hello, User');
+    }
+
+    public function loadProductsPageAsTestUserTest(AcceptanceTester $I)
+    {
+        $I->wantTo('Log in as a test user, and then view the products page.');
+        $this->loginAsTestUser($I);
+        $I->wait(1);
+        $I->click('PRODUCTS');
+        $I->wait(1);
+        $I->see('Price');
+        $I->see('Brands');
     }
 }
