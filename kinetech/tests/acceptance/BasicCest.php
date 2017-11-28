4f43036fd5c69bccf52d6756c92597cbc04c1f35
Elliott Allmann <elliott.allmann@cox.net>
2017-11-27 18:56:51 -0800

    First tests written!

--- kinetech/tests/acceptance/BasicCest.php
+++ kinetech/tests/acceptance/BasicCest.php
@@ -16,7 +16,22 @@ class BasicCest
     {
         $I->wantTo("Make sure the home page loads");
         $I->amOnPage('/');
-        $I->wait(2);
+        $I->wait(1);
         $I->see("Kinetech");
+    }
+
+    public function loginTest(AcceptanceTester $I)
+    {
+        $I->wantTo("Attempt to log in with good data.");
+        $I->amOnPage('/');
+        $I->click('LOGIN');
+        $I->wait(1);
+        $I->fillField('email', 'testuser@test.com');
+        $I->fillField('password', 'testpassword');
+        $I->click('Log In');
+        $I->wait(2);
+        $I->see('CART');
+        $I->see('PROFILE');
+        $I->see('LOG OUT');
     }
 }
